3a81e6e991bced5e24465951f8406ea87a4e3161
root-deva <devavratha.nc@gmail.com>
2026-07-31 20:01:48 +0530

    Module 1 - Initial ASP.NET Core MVC Project setup

---
+++ Order-Tracking-System.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Order_Tracking_System.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
